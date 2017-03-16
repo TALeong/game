4d55ead18455194e61f6e9548922cb937e396468
ElisaZhang <elisa_zhang@student.wayland.k12.ma.us>
2017-03-16 08:16:46 -0400

    fislfjskdfjlskdf

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import javax.swing.*;
 
 public class Main extends JPanel{
 
-
-
     public static final int FRAMEWIDTH = 1400, FRAMEHEIGHT = 800;
+
+    //delete
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Dodgeeeeeeeeeeeeeee");
