d9c3b0965bf9d5cc7c5a4fb9d071bcb0c31a014a
ElisaZhang <elisa_zhang@student.wayland.k12.ma.us>
2017-03-16 08:14:15 -0400

    fislfjskdfjlskdf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JPanel{
+
+
+
+    public static final int FRAMEWIDTH = 1400, FRAMEHEIGHT = 800;
+
     public static void main(String[] args) {
-        System.out.println("HI!");
+        JFrame window = new JFrame("Dodgeeeeeeeeeeeeeee");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+        Main panel = new Main();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
     }
 }
