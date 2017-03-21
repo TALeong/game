ca5548eb6ed662c2a11c92c570cf5d4a405cd52e
talia_leong <talialeong@gmail.com>
2017-03-21 12:53:00 -0400

    HI

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,18 @@
 import java.awt.*;
 
 public class Player extends Sprite{
+    private int hp;
     public Player() {
         super(new Point(Main.FRAMEWIDTH/2, Main.FRAMEHEIGHT/2), NORTH);
         setPic("player.png", NORTH);
+        hp = 5;
+    }
+
+    public void setHp(int n) {
+        hp = n;
+    }
+
+    public int getHp() {
+        return hp;
     }
 }
