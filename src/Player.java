d866edf9cec38e357cf5d7a72cd20f597f119812
ElisaZhang <elisa_zhang@student.wayland.k12.ma.us>
2017-03-21 13:05:56 -0400

    fislfjskdfjlskdffdfdfdf

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,25 @@
 import java.awt.*;
 
 public class Player extends Sprite{
+    private boolean isDead;
+
     public Player() {
         super(new Point(Main.FRAMEWIDTH/2, Main.FRAMEHEIGHT/2), NORTH);
         setPic("player.png", NORTH);
     }
 }
+    public void update(){
+        if (!isDead)
+            super.update();
+        else {
+            getDead();
+            setDead(true);
+        }
+    }
+
+    public void setDead(boolean d){
+        isDead=d;
+    }
+    public boolean getDead(){
+        return isDead;
+    }
