c8dcfdd8892c34b6c84b1d541b95e8bdc905987d
ElisaZhang <elisa_zhang@student.wayland.k12.ma.us>
2017-03-21 12:50:17 -0400

    fislfjskdfjlskdffdfdfdf

---
+++ src/Enemies.java
@@ -0,0 +1,7 @@
+/**
+ * Created by elisa_zhang on 3/21/17.
+ */
+public class Enemies extends Sprite {
+
+    Enemies
+}
--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,8 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
 public class Main extends JPanel{
+
     public static final int FRAMEWIDTH = 1400, FRAMEHEIGHT = 800;
-
-    public Main() {
-
-        addMouseListener(new MouseListener() {
-            public void mouseClicked(MouseEvent mouseEvent) {
-
-            }
-
-            public void mousePressed(MouseEvent mouseEvent) {
-
-            }
-
-            public void mouseReleased(MouseEvent mouseEvent) {
-
-            }
-
-            public void mouseEntered(MouseEvent mouseEvent) {
-
-            }
-
-            public void mouseExited(MouseEvent mouseEvent) {
-
-            }
-        });
-
-        addMouseMotionListener(new MouseMotionListener() {
-            public void mouseDragged(MouseEvent mouseEvent) {
-
-            }
-
-            public void mouseMoved(MouseEvent mouseEvent) {
-
-            }
-        });
-
-        addKeyListener(new KeyListener() {
-            public void keyTyped(KeyEvent keyEvent) {
-
-            }
-
-            public void keyPressed(KeyEvent keyEvent) {
-
-            }
-
-            public void keyReleased(KeyEvent keyEvent) {
-
-            }
-        });
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
-    }
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Dodgeeeeeeeeeeeeeeee");
