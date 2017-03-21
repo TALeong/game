3695b4ad20e404ab195bb3cc98875a86e251e6db
talia_leong <talialeong@gmail.com>
2017-03-21 12:41:07 -0400

    HI

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,64 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Main extends JPanel{
 
     public static final int FRAMEWIDTH = 1400, FRAMEHEIGHT = 800;
+
+    public Main() {
+
+        addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent mouseEvent) {
+
+            }
+
+            public void mousePressed(MouseEvent mouseEvent) {
+
+            }
+
+            public void mouseReleased(MouseEvent mouseEvent) {
+
+            }
+
+            public void mouseEntered(MouseEvent mouseEvent) {
+
+            }
+
+            public void mouseExited(MouseEvent mouseEvent) {
+
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionListener() {
+            public void mouseDragged(MouseEvent mouseEvent) {
+
+            }
+
+            public void mouseMoved(MouseEvent mouseEvent) {
+
+            }
+        });
+
+        addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent keyEvent) {
+
+            }
+
+            public void keyPressed(KeyEvent keyEvent) {
+
+            }
+
+            public void keyReleased(KeyEvent keyEvent) {
+
+            }
+        });
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+    }
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Dodgeeeeeeeeeeeeeeee");
@@ -12,7 +68,7 @@ public class Main extends JPanel{
         Main panel = new Main();
         panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
 
-        panel.setFocusable(true);
+//        panel.setFocusable(true);
         panel.grabFocus();
 
         window.add(panel);
