897fbca9987e15d02370d33ff50e4f1b2dfafc47
talia_leong <talialeong@gmail.com>
2017-03-22 08:04:23 -0400

    HI

--- src/Enemies.java
+++ src/Enemies.java
@@ -4,11 +4,16 @@ import java.awt.*;
  * Created by elisa_zhang on 3/21/17.
  */
 public class Enemies extends Sprite {
-
-    public Enemies(Point loc, int dir){
+    private int hp;
+    public Enemies(Point loc, int dir) {
         super(loc, dir);
         setPic("enemies.png", NORTH);
         setSpeed(0);
     }
-
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int n) {
+        hp = n;
+    }
 }
