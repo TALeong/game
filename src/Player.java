8d3d7bb96e8e4edf7256aa1ff29249cdbac96961
talia_leong <talialeong@gmail.com>
2017-03-27 09:14:30 -0400

    HI

--- src/Player.java
+++ src/Player.java
@@ -5,6 +5,7 @@ public class Player extends Sprite{
     public Player() {
         super(new Point(Main.FRAMEWIDTH/2, Main.FRAMEHEIGHT/2), NORTH);
         setPic("player.png", NORTH);
+        setSpeed(10);
         hp = 5;
     }
 
